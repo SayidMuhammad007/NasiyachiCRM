75c44079ae9295aef217bba72dcf7c09a2d5f57e
SayidMuhammad Jabborov <122009997+SayidMuhammad007@users.noreply.github.com>
2023-10-24 18:15:43 +0500

    Update notification.py

--- notification.py
+++ notification.py
@@ -2,13 +2,17 @@ import logging
 from aiogram.types import ParseMode
 
 from loader import bot
-from sheet import getData1,  getAll
+from sheet import getData1, getAll, add_row
 
 USER_ID = 5333351384
+
+from aiogram.utils.exceptions import ChatNotFound
 
 async def send_notification(user_id, message_text):
     try:
         await bot.send_message(user_id, message_text, parse_mode=ParseMode.HTML)
+    except ChatNotFound:
+        logging.warning(f"Chat not found for user_id: {user_id}")
     except Exception as e:
         logging.exception(e)
 
@@ -28,6 +32,7 @@ async def monitor_database():
 😎 Hamkorni tasdiqlab yuborilar hamkasblar!
                 """
                 await send_notification(i[4], text)
+                await add_row([["BA", int(data[0][0]) + 3, 0]], table="🏢 Hamkor-do'konlar")
                 logging.exception("Sended")
             else:
                 logging.exception("Nothing to send")
